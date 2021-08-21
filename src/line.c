862542732fb3ff7f8dffe42f36da47bcd25e65d2
Shoumodip Kar <shoumodipkar@gmail.com>
2021-08-21 11:34:28 +0530

    Implement line_split()

--- src/line.c
+++ src/line.c
@@ -40,6 +40,17 @@ void line_insert(Line *line, size_t index, char *string, size_t count)
 }
 
 /*
+ * Insert a string at the end of a line
+ * @param line *Line The line to insert the string in
+ * @param string *char The string to insert
+ * @param count size_t The number of bytes in the string to insert
+ */
+void line_append(Line *line, char *string, size_t count)
+{
+    line_insert(line, line->length, string, count);
+}
+
+/*
  * Delete characters from a point in a line
  * @param line *Line The line to delete the characters in
  * @param index size_t The index to delete the characters from
@@ -47,8 +58,29 @@ void line_insert(Line *line, size_t index, char *string, size_t count)
  */
 void line_delete(Line *line, size_t index, size_t count)
 {
-    if (index + count < line->length)
-        memmove(line->text + index, line->text + index + count, count);
+    if (index + count < line->length) {
+        memmove(line->text + index,
+                line->text + index + count,
+                line->length - count);
+    }
 
     line->length -= count;
+}
+
+/*
+ * Split a line at a particular index
+ * @param line *Line The line to split
+ * @param index size_t The index to split the line at
+ * @return *Line The left hand side of the split line
+ */
+Line line_split(Line *line, size_t index)
+{
+    Line new = {0};
+
+    if (index < line->length) {
+        line_append(&new, line->text + index, line->length - index);
+        line->length = index;
+    }
+
+    return new;
 }
--- src/line.h
+++ src/line.h
@@ -15,5 +15,7 @@ typedef struct {
 void line_free(Line *line);
 void line_insert(Line *line, size_t index, char *string, size_t length);
 void line_delete(Line *line, size_t index, size_t count);
+void line_append(Line *line, char *string, size_t count);
+Line line_split(Line *line, size_t index);
 
 #endif // LINE_H
